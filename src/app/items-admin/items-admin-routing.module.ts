51bf9599ce33b9d5c4b88729e3a9b4d4969ee7fd
Rózsa Patrik Zsolt <h163488@stud.u-szeged.hu>
2023-04-11 11:18:09 +0200

    admin items routing updated

--- src/app/items-admin/items-admin-routing.module.ts
+++ src/app/items-admin/items-admin-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemsAdminComponent } from './items-admin.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [{path: '', component: ItemsAdminComponent}]
 
 @NgModule({
   declarations: [],
