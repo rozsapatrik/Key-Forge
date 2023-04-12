337b911bf2102785cfbf46eba1606b6fb9a53459
Rózsa Patrik Zsolt <h163488@stud.u-szeged.hu>
2023-04-12 08:49:06 +0200

    admin and user guard is now done

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../shared/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+
+  constructor(private userv: UserService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+    const user = JSON.parse(localStorage.getItem("user") as string);
+    if (user) {
+      return this.router.createUrlTree(["/home"]);
+    }
+    else {
+      return true;
+    }
   }
   
 }
