c97b1b9ca6cc9a32ca0a0f5d2ffa1f3d45df166a
Rózsa Patrik Zsolt <h163488@stud.u-szeged.hu>
2023-04-12 08:33:42 +0200

    very basics of homepage has been set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CartService } from './shared/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'key-forge';
+
+  constructor(private cartserv: CartService) {}
+
+  onToggleSidenav(sidenav: MatSidenav){
+    sidenav.toggle();
+  }
+
+  onClose(event: any, sidenav: MatSidenav) {
+    if (event === true) {sidenav.close();}
+  }
+
+  ngOnInit() {
+    //this.cartserv.ngOnInit();
+  }
+
+  ngOnChanges(): void {
+  }
 }
