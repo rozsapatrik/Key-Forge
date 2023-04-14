71eee2df49a576e95bb165419b4bc2b85297dc7d
Rózsa Patrik Zsolt <h163488@stud.u-szeged.hu>
2023-04-14 09:36:17 +0200

    login function is now implemented and can be used

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, Output } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../shared/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from '../shared/user.module';
 
 @Component({
@@ -6,6 +12,51 @@ import { User } from '../shared/user.module';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   @Output() loggedUserOutput?: User;
+
+  constructor(private userv: UserService, private router: Router, private toastr: ToastrService, private firestore: AngularFirestore, private auth: AngularFireAuth) { }
+
+
+  ngOnInit(): void {
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [
+      Validators.email
+    ]),
+    password: new FormControl(null, [
+      Validators.minLength(6)
+    ]),
+  });
+
+  async onSubmit() {
+    localStorage.setItem("admin", JSON.stringify("false"));
+    const loginData = this.loginForm.value;
+
+    if (this.loginForm.valid) {
+      this.userv.login(loginData.email as unknown as string, loginData.password as unknown as string).then(cred => {
+        this.toastr.success("Logged in successfully!", "Login");
+        this.router.navigateByUrl("home");
+
+        this.auth.onAuthStateChanged((user) => {
+          if (user) {
+
+            this.firestore.collection("users").doc(user.uid).valueChanges().subscribe(v => {
+              this.userv.userData = v as User;
+              if (this.userv.userData.isAdmin) {
+                localStorage.setItem("admin", JSON.stringify("true"));
+              }
+              else {
+                localStorage.setItem("admin", JSON.stringify("false"));
+              }
+            });
+          }
+        });
+
+      }).catch(error => {
+        this.toastr.error("Email or password is incorrect!", "Login error");
+      });
+    }
+  }
 }
